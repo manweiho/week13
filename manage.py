0dea9fda8053ffeed1b111e1cb8de3074b3e071b
manweiho <82921578+manweiho@users.noreply.github.com>
2023-04-29 14:24:24 -0400

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,54 +1,52 @@
-from flask import Flask, url_for, redirect, render_template, request
-from flask_bootstrap import Bootstrap
-
+rom flask import Flask, render_template, request
 import sqlite3 as sql
-app = Flask(__name__)
-Bootstrap(app)
+app = Flask(__name__, template_folder = '/home/ubuntu/templates/')
 
 @app.route('/')
 def home():
-   return render_template('home.htm')
+   return render_template('home.html')
 
 @app.route('/enternew')
 def new_employee():
-   return render_template('employee.htm')
+   return render_template('employee.html')
 
 
 @app.route('/registration',methods = ['POST', 'GET'])
 def addrec():
    if request.method == 'POST':
       try:
-         id = request.form['id']
-         name = request.form['nm']
-         gender = request.form['g']
-         phone = request.form['phone']
-         birthdate = request.form['date']
-         
-         with sql.connect("employees.db") as con:
+         EmpID = request.form['id']
+         EmpName = request.form['nm']
+         EmpGender = request.form['g']
+         EmpPhone = request.form['phone']
+         EmpBdate = request.form['date']
+
+         with sql.connect("/home/ubuntu/week13_flask/employees.db") as con:
             cur = con.cursor()
-            cmd = "INSERT INTO employees (EmpID,EmpName,EmpGender,EmpPhone,EmpBdate) VALUES ('{0}','{1}','{2}','{3}',{4}')".format(id,nm,g,phone,date)
+            cmd = "INSERT INTO employees (EmpID,EmpName,EmpGender,EmpPhone,EmpBdate) VALUES ('{0}','{1}','{2}','{3}','{4}')".format(EmpID,EmpName,EmpGender,EmpPhone,EmpBdate)
             cur.execute(cmd)
-            
+
             con.commit()
             msg = "Record successfully added"
       except:
          con.rollback()
          msg = "error in insert operation"
-         
+
       finally:
-         return render_template("output.htm",msg = msg)
+         return render_template("result.html",msg = msg)
          con.close()
 
 @app.route('/information')
 def information():
-   con = sql.connect("employees.db")
+   con = sql.connect("/home/ubuntu/week13_flask/employees.db")
    con.row_factory = sql.Row
    
    cur = con.cursor()
    cur.execute("select * from employees")
    
    rows = cur.fetchall(); 
-   return render_template("information.htm",rows = rows)
+   return render_template("information.html",rows = rows)
 
 if __name__ == '__main__':
    app.run(debug = True)
+
