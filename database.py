8449b27b58d665395e49c4d9ec9a94394d66ba3e
manweiho <82921578+manweiho@users.noreply.github.com>
2023-04-29 14:23:32 -0400

    Update database.py

--- database.py
+++ database.py
@@ -1,10 +1,10 @@
-pip install sqlite3
 import sqlite3
 
-conn = sqlite3.connect('employees.db')
+conn = sqlite3.connect('/home/ubuntu/week13_flask/employees.db')
+print("Opened database successfully")
 
-conn.execute('CREATE TABLE employees (EmpID TEXT, EmpName TEXT, EmpGender TEXT, EmpPhone TEXT, EmpBdate DATE)')
-
+conn.execute('CREATE TABLE employees (EmpID TEXT, EmpName TEXT, EmpGender TEXT, EmpPhone TEXT, EmpBdate TEXT)')
 print("Table created successfully")
+
 
 conn.close()
